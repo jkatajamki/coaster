edf8d87f8c0727d5aa8d043b25fa2068665640ae
Johannes Katajamäki <jkatajamki@gmail.com>
2020-02-09 21:19:34 +0200

    Add logger

---
+++ src/server/lib/logging/logger.ts
@@ -0,0 +1,42 @@
+import { transports, createLogger, format } from 'winston'
+import expressWinston from 'express-winston'
+import { TransformableInfo } from 'logform'
+
+const LOG_DIR = `${__dirname}/../../../logs/`
+
+const printFormat = (info: TransformableInfo) =>
+  `${info.timestamp} - ${info.level}: ${info.message}`
+
+const formatConfig = {
+  default: format.combine(
+    format.timestamp(),
+    format.colorize(),
+    format.printf(printFormat)
+  ),
+}
+
+const transportsConfig = {
+  default: [
+    new transports.Console(),
+    new transports.File({ filename: `${LOG_DIR}/debug.log` }),
+  ],
+}
+
+const exceptionHandlersConfig = {
+  default: [
+    new transports.Console(),
+    new transports.File({ filename: `${LOG_DIR}/error.log` }),
+  ],
+}
+
+const logger = createLogger({
+  level: 'info',
+  format: formatConfig.default,
+  transports: transportsConfig.default,
+  exceptionHandlers: exceptionHandlersConfig.default,
+  exitOnError: false,
+})
+
+export default logger
+
+export const loggerMiddleware = expressWinston.logger({ winstonInstance: logger })
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,5 +1,17 @@
-const world = '🗺️'
+import express from 'express'
+import bodyParser from 'body-parser'
+import config from 'config'
+import compression from 'compression'
+import logger, { loggerMiddleware } from './lib/logging/logger'
 
-export default (word: string = world): string => {
-  return `Hello ${word}!`
-}
+const port: string = config.get('port')
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(compression())
+app.use(loggerMiddleware)
+
+app.listen(port, () => {
+  logger.info(`Server listening on port ${port} 💿 🎜 🎝`)
+})
